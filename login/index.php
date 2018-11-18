216247ba9ea9d6cfe80f26f84493be6e18cd3172
Gatozaurio <tavoraromero@gmail.com>
2018-11-18 23:45:01 +0100

    4.4 Envía consultas con los datos del login

--- login/index.php
+++ login/index.php
@@ -26,21 +26,31 @@
             $sql = "SELECT * FROM users WHERE username = '$username' LIMIT 1";
             // lanza una consula contra la bd |conexión|consulta|
             $login = mysqli_query($db, $sql);
+            $ip = $_SERVER['REMOTE_ADDR'];
 
             if($login && mysqli_num_rows($login) == 1 ){
 
                 $usuario = mysqli_fetch_assoc($login);
 
                 if( password_verify($password, $usuario['password']) ){
+                    // Manda una consulta con los datos de la conexión exitosa
+                    $query = "INSERT into logins VALUES(NULL, true, 'N/A', '$ip', '$username',NOW() )";
+                    mysqli_query($db, $query);
                     // Se crea la sesión de usuario
                     session_start();
                     // Se asignan los datos de usuario a la sesión userdata
                     $_SESSION['userdata'] = $usuario;
                     header('Location: '.APP_URL);
                 }else{
+                    // Manda una consulta avisando de la conexión errónea por haber introducido una contraseña incorrecta
+                    $query = "INSERT into logins VALUES(NULL, false, 'password', '$ip', '$username',NOW() )";
+                    mysqli_query($db, $query);
                     $errors['login']['password'] = "La contraseña no es correcta";
                 }
             }else{
+                // Manda una consulta avisando de la conexión errónea por haber introducido un usuario incorrecto
+                $query = "INSERT into logins VALUES(NULL, false,'username', '$ip', '$username',NOW() )";
+                mysqli_query($db, $query);
                 $errors['login']['username'] = "El usuario es incorrecto";
             }
 
